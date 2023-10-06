44f1f95b7ed9da3a076ed713290d594de7f05ed8
tgprudencio <thiago.gprudencio@gmail.com>
2023-10-06 15:06:48 -0300

    refactor: remove unused comments

--- src/server.js
+++ src/server.js
@@ -2,14 +2,3 @@ import app from './app';
 
 app.listen(3333);
 
-/*
-const express = require('express');
-
-const server = express();
-
-server.get('/teste', (req, res) => {
-    return res.json({ message: "Hello" })
-})
-
-server.listen(3000)
-*/
