cc54aada1038db2c4f2610d8b97408bd9f7e58f6
tgprudencio <thiago.gprudencio@gmail.com>
2023-10-14 17:45:45 -0300

    refactor: update database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
-  database: 'postgres',
+  password: 'godontodeploy',
+  database: 'godonto',
   define: {
     timestamps: true,
     underscore: true,
